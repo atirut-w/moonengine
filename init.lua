a8cae73b31e0ad05e818f9a23436ed8d6e84e8b7
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2021-12-25 00:51:41 +0700

    Node system

--- init.lua
+++ init.lua
@@ -1,5 +1,27 @@
+local moonengine = {}
 local log = require("moonengine.log")
 
-function love.load()
-    log.info("Hello MoonEngine!")
+local rootnode = require("moonengine.node")("root")
+
+local function process_nodes(node)
+    if node.update then
+        local success, result = pcall(node.update, node)
+        if not success then
+            log.error("Error in node update: " .. result)
+        end
+    end
+
+    for _, child in ipairs(node.children) do
+        process_nodes(child)
+    end
 end
+
+function moonengine.get_root()
+    return rootnode
+end
+
+function moonengine.update()
+    process_nodes(rootnode)
+end
+
+return moonengine
---
+++ node.lua
@@ -0,0 +1,32 @@
+local object = require("moonengine.object")
+local log = require("moonengine.log")
+
+local node = object:extend()
+
+function node:new(name)
+    self.name = name or "node"
+    self.parent = nil
+    self.children = {}
+
+    log.info("Created node: " .. self.name)
+end
+
+function node:add_child(child)
+    assert(child.is, "child is not a valid class")
+    assert(child:is(node), "child is not or does not inherit from node")
+
+    child.parent = self
+    table.insert(self.children, child)
+
+    if child.init then
+        local success, result = pcall(child.init, child)
+        if not success then
+            log.error("Failed to initialize node " .. child.name .. ": " .. result)
+        end
+    end
+
+    log.info("Added child " .. child.name .. " to node " .. self.name)
+    return child
+end
+
+return node
