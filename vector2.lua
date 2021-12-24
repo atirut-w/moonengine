f7c326885149a02eca02d875b215a77d6c4d13c3
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2021-12-25 05:06:51 +0700

    Metamethods for `vector2`

--- vector2.lua
+++ vector2.lua
@@ -6,4 +6,24 @@ function vector2:new(x, y)
     self.y = y or 0
 end
 
+function vector2:__add(other)
+    return vector2(self.x + other.x, self.y + other.y)
+end
+
+function vector2:__sub(other)
+    return vector2(self.x - other.x, self.y - other.y)
+end
+
+function vector2:__mul(other)
+    return vector2(self.x * other, self.y * other)
+end
+
+function vector2:__div(other)
+    return vector2(self.x / other, self.y / other)
+end
+
+function vector2:__eq(other)
+    return self.x == other.x and self.y == other.y
+end
+
 return vector2
