d38c47d2d8f3964eadaad5efa7a6abe8c4b9db2e
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2021-12-25 05:31:46 +0700

    `length()` and `normalize()` for `Vector2`

--- vector2.lua
+++ vector2.lua
@@ -6,6 +6,10 @@ function vector2:new(x, y)
     self.y = y or 0
 end
 
+function vector2:length()
+    return math.sqrt(self.x * self.x + self.y * self.y)
+end
+
 function vector2:__add(other)
     return vector2(self.x + other.x, self.y + other.y)
 end
@@ -24,6 +28,22 @@ end
 
 function vector2:__eq(other)
     return self.x == other.x and self.y == other.y
+end
+
+function vector2:__lt(other)
+    return self:length() < other:length()
+end
+
+function vector2:__le(other)
+    return self:length() <= other:length()
+end
+
+function vector2:normalize()
+    local length = self:length()
+    if length == 0 then
+        return vector2()
+    end
+    return vector2(self.x / length, self.y / length)
 end
 
 return vector2
