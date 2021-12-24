8cec8b13310b094c63f028bf1861de7fa4772238
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2021-12-25 02:38:31 +0700

    Upgrade `process_nodes` into `traverse_nodes`

--- init.lua
+++ init.lua
@@ -3,16 +3,11 @@ local log = require("moonengine.log")
 
 local rootnode = require("moonengine.node")("root")
 
-local function process_nodes(node)
-    if node.update then
-        local success, result = pcall(node.update, node)
-        if not success then
-            log.error(("Error while updating node %s: %s"):format(node.name, result or "unknown error"))
-        end
-    end
+local function traverse_nodes(node, handler)
+    handler(node)
 
     for _, child in ipairs(node.children) do
-        process_nodes(child)
+        traverse_nodes(child, handler)
     end
 end
 
@@ -21,7 +16,14 @@ function moonengine.get_root()
 end
 
 function moonengine.update()
-    process_nodes(rootnode)
+    traverse_nodes(rootnode, function(node)
+        if node.update then
+            local success, result = pcall(node.update, node)
+            if not success then
+                log.error(("Error while updating node %s: %s"):format(node.name, result or "unknown error"))
+            end
+        end
+    end)
 end
 
 return moonengine
